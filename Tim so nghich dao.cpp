434e2935044cebe05bfb7913d8d086f9d1a83411
Blackhacker-a <64885014+Blackhacker-a@users.noreply.github.com>
2020-05-12 20:32:44 +0700

    Create Tim so nghich dao.cpp

---
+++ Tim so nghich dao.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h> //Tim so nghich dao tong quat
+int main(){
+	int n;
+	int r,dao=0;
+	printf("Nhap vao so nguyen n ");
+	scanf("%d",&n);
+	
+	while(n!=0){
+	    r=n%10;
+		dao=dao*10+r;
+		n=n/10;
+		}
+	printf("So dao la %d",dao);	
+	
+	
+	
+	
+	}
+	
+
+
+
+
+
+
+
+
+
+
+}	
